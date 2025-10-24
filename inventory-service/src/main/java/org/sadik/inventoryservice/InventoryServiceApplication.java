289ca24e2744dc8b794f6a73808fcaa7d1064730
SADIK Oussama <Oussama.sadik@emsi-edu.ma>
2025-10-24 22:55:21 +0100

    Inventory-service

--- inventory-service/src/main/java/org/sadik/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sadik/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,49 @@
 package org.sadik.inventoryservice;
 
+import org.sadik.inventoryservice.entities.Product;
+import org.sadik.inventoryservice.repository.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(ProductRepository productRepository){
+		return args -> {
+			productRepository.save(Product.builder()
+					.id(UUID.randomUUID().toString())
+					.name("Computer")
+					.price(6500)
+					.quantity(321)
+					.build());
+			productRepository.save(Product.builder()
+					.id(UUID.randomUUID().toString())
+					.name("Printer")
+					.price(5400)
+					.quantity(19)
+					.build());
+			productRepository.save(Product.builder()
+					.id(UUID.randomUUID().toString())
+					.name("Smart Phone")
+					.price(4300)
+					.quantity(14)
+					.build());
+
+			productRepository.findAll().forEach(p->{
+				System.out.println(p.toString());
+			});
+		};
 	}
 
 }
---
+++ inventory-service/src/main/java/org/sadik/inventoryservice/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package org.sadik.inventoryservice.repository;
+
+
+import org.sadik.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,String> {
+}
